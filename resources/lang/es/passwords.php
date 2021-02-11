c1a86d6918846eb566ba7f7b57bf850e2ce5559d
snaven10 <frjsamuel@gmail.com>
2021-02-10 22:17:06 -0600

    Crete proyect Reliance laravel

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Su contrasena fue resestablecida!',
+    'sent' => 'Enviamos un correo con el enlace de restablecimiento de contrasena!',
+    'throttled' => 'Por favor espere un momento para intentar de nuevo.',
+    'token' => 'El token de restablecimiento de contrasena es invalido.',
+    'user' => "No se ha encontrado ningun usuario con este correo electronico",
+
+];
